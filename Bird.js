3a7f672390308f83441952b14b7f14c0b131bf11
kdeepthi123 <44495139+kdeepthi123@users.noreply.github.com>
2021-05-29 22:27:36 +0530

    added angry birds code

---
+++ Bird.js
@@ -0,0 +1,22 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.trajectory = [];
+    this.smokeImg=loadImage('sprites/smoke.png')
+    this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if(this.body.speed>3 && this.body.position.x>200){
+      this.trajectory.push([this.body.position.x,this.body.position.y])
+    }
+    console.log(this.trajectory)
+   for(var i=0;i<this.trajectory.length;i++){
+     image(this.smokeImg,this.trajectory[i][0],this.trajectory[i][1])
+   }
+
+    super.display();
+  }
+}
